8fada2a960c3be81bbb42c66bb8708f73ac9939d
Danielandro <mtme13@hotmail.com>
2019-11-18 23:36:20 +0000

    Grab user input

--- calculator/src/main/java/com/danny/calculator/App.java
+++ calculator/src/main/java/com/danny/calculator/App.java
@@ -1,5 +1,7 @@
 package com.danny.calculator;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
@@ -9,9 +11,18 @@ public final class App {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+        getUserInput();
+    }
+
+    public static void getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        scanner.close();
+        System.out.println(userInput);
     }
 }
